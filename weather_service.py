f66d4780f0c5f8e26ec9bcbffca4f37d328882f2
Paddu Melanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2026-03-17 19:48:31 -0700

    Implement mock for wind speed in tests
    Add mock for get_wind_speed in hurricane alert test

--- weather_service.py
+++ weather_service.py
@@ -12,6 +12,26 @@ def test_hurricane_alert_trigger(mock_wind):
 
     # Even if the real weather is sunny, our test "sees" a hurricane
     assert check_hurricane_safety() == "EVACUATE"
+"""
+patch lets you replace a real function with a fake one
+This fake version is called a mock
+@patch('weather_service.get_wind_speed')
+What this does:
+
+“Temporarily replace get_wind_speed with a mock object”
+
+Only during this test
+
+After the test → everything goes back to normal
+
+def test_hurricane_alert_trigger(mock_wind):
+
+mock_wind is the fake version of get_wind_speed
+
+It’s automatically passed in by patch
+
+"""
+
 
 #weather_service.py
 def get_wind_speed():
