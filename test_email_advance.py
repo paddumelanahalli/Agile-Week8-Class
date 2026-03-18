b375ff92fd278b19c60afed5b8ff5fa78800c6e0
Paddu Melanahalli <47433437+paddumelanahalli@users.noreply.github.com>
2026-03-17 19:32:06 -0700

    Refactor get_valid_emails for memory efficiency
    Refactored get_valid_emails to use a generator expression for improved memory efficiency when handling large email lists.

--- test_email_advance.py
+++ test_email_advance.py
@@ -9,6 +9,25 @@ from email_tools import get_valid_emails
     ([], []),                                             # Empty case
     ([123, None, "test@test.com"], ["test@test.com"]),    # Dirty data case
 ])
+
 def test_email_filtering_at_scale(input_list, expected_output):
     # This single line now runs 4 different tests!
     assert get_valid_emails(input_list) == expected_output
+
+#Refractor method - in the real world checking isinstance and in inside a list 
+#comprehension is fine for 10 emails but what about 10 million?
+# We can refractor to use Generator Expression for better memory efficiency
+
+def get_valid_emails(emails):
+    """ 
+    Refractored to handle potential memory leaks by using
+    a generator for massive database 
+    """
+    if not isinstance(emails,list):
+        raise TypeError("Input must be a list of strings")
+    """
+    using a generator ensures we don't build the whole list
+    in memory if we are just iterating over it later
+    """
+    valid_generator = (e for e in emails if isinstance(e,str) and "@" in e)
+    return list(valid_generator)
